427a1cc78569fcc75a4be1d95ee3f405a62fa273
irfanshah22 <45657454+irfanshah22@users.noreply.github.com>
2024-03-07 11:25:43 +0500

    effect on mouse/touch click area

--- Assets/RippleEffect/RippleEffect.cs
+++ Assets/RippleEffect/RippleEffect.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class RippleEffect : MonoBehaviour
@@ -30,6 +31,11 @@ public class RippleEffect : MonoBehaviour
 
     [Range(0.0f, 2.0f)]
     public float dropInterval = 0.5f;
+    private float _tempdropInterval = 0f;
+
+    public static Vector3 MousePos;
+    public Text _androidText;
+    private int Counter;
 
     [SerializeField, HideInInspector]
     Shader shader;
@@ -57,8 +63,11 @@ public class RippleEffect : MonoBehaviour
 
         public Vector4 MakeShaderParameter(float aspect)
         {
-            return new Vector4(position.x * aspect, position.y, time, 0);
-        }
+            //  (-0.3, 0.4, 0.0)
+          //  (0.3, 0.5, 0.0)
+           // return new Vector4(position.x * aspect, position.y, time, 0);
+            return new Vector4(MousePos.x+.9f, MousePos.y+.5f, time, 0);   
+        } 
     }
 
     Droplet[] droplets;
@@ -66,14 +75,14 @@ public class RippleEffect : MonoBehaviour
     Material material;
     float timer;
     int dropCount;
-
+    private bool _MouseHit;
     void UpdateShaderParameters()
     {
         var c = GetComponent<Camera>();
 
         material.SetVector("_Drop1", droplets[0].MakeShaderParameter(c.aspect));
-        material.SetVector("_Drop2", droplets[1].MakeShaderParameter(c.aspect));
-        material.SetVector("_Drop3", droplets[2].MakeShaderParameter(c.aspect));
+        //material.SetVector("_Drop2", droplets[1].MakeShaderParameter(c.aspect));
+        //material.SetVector("_Drop3", droplets[2].MakeShaderParameter(c.aspect));
 
         material.SetColor("_Reflection", reflectionColor);
         material.SetVector("_Params1", new Vector4(c.aspect, 1, 1 / waveSpeed, 0));
@@ -82,10 +91,13 @@ public class RippleEffect : MonoBehaviour
 
     void Awake()
     {
-        droplets = new Droplet[3];
+        _tempdropInterval = dropInterval;
+        _MouseHit = false;
+        Counter = 0;
+        droplets = new Droplet[1]; 
         droplets[0] = new Droplet();
-        droplets[1] = new Droplet();
-        droplets[2] = new Droplet();
+        //droplets[1] = new Droplet();
+        //droplets[2] = new Droplet();
 
         gradTexture = new Texture2D(2048, 1, TextureFormat.Alpha8, false);
         gradTexture.wrapMode = TextureWrapMode.Clamp;
@@ -96,17 +108,72 @@ public class RippleEffect : MonoBehaviour
             var a = waveform.Evaluate(x);
             gradTexture.SetPixel(i, 0, new Color(a, a, a, a));
         }
-        gradTexture.Apply();
+        gradTexture.Apply();    
 
         material = new Material(shader);
         material.hideFlags = HideFlags.DontSave;
-        material.SetTexture("_GradTex", gradTexture);
-
-        UpdateShaderParameters();
+        material.SetTexture("_GradTex", gradTexture);  
+        // UpdateShaderParameters();
     }
 
     void Update()
     {
+#if UNITY_EDITOR_WIN
+         if (Input.GetMouseButtonDown(0))
+        {
+            _androidText.text = "Mouse Clicked";
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                _androidText.text = "Mouse Hit editor or PC";
+
+                if (hit.collider.gameObject.tag == "Plane")
+                {
+                    Counter += 1;
+                     _androidText.text = "Plane Hited  = " + Counter;
+                    print(hit.point);
+                     print(hit.collider.gameObject.name);
+                    MousePos = hit.point;
+                    _MouseHit = true;
+                    dropInterval = _tempdropInterval;
+                    StopCoroutine(Stopbool());
+                    StartCoroutine(Stopbool()); 
+                  }
+            }
+        }
+#endif
+#if UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+              if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Touch touch = Input.GetTouch(0);
+                // Construct a ray from the current touch coordinates
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                // Create a particle if hit
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {  
+                    if (hit.collider.gameObject.tag == "Plane")
+                    {
+                        Counter += 1;
+                        _androidText.text = "touch plane hited Android= " + Counter;
+                         print(hit.point);
+                          MousePos = hit.point;
+                        _MouseHit = true;
+                        dropInterval = _tempdropInterval;  
+                        StopCoroutine(Stopbool());
+                        StartCoroutine(Stopbool());  
+                    }   
+                }
+            }
+        }
+#endif
+
+        if (!_MouseHit) 
+            return; 
         if (dropInterval > 0)
         {
             timer += Time.deltaTime;
@@ -117,10 +184,15 @@ public class RippleEffect : MonoBehaviour
             }
         }
 
-        foreach (var d in droplets) d.Update();
+        foreach (var d in droplets) d.Update(); 
 
         UpdateShaderParameters();
     }
+    IEnumerator Stopbool()
+    {
+        yield return new WaitForSeconds(1);
+        dropInterval = 0;
+    } 
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
